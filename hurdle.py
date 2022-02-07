35f5f852bbd4a2b580910afeb6a85a227aff3ac5
CocoLu-777 <71462364+CocoLu-777@users.noreply.github.com>
2022-02-07 14:36:29 -0800

    Update hurdle.py

--- hurdle.py
+++ hurdle.py
@@ -19,3 +19,21 @@ while not at_goal():
         jump()
     else:
         move()
+        
+        
+        
+# random position to start
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
+    
