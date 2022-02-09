4a3383b91c15190cf04f3e32344a3375af68ac71
CocoLu-777 <71462364+CocoLu-777@users.noreply.github.com>
2022-02-08 22:59:16 -0800

    Create caesar_cipher_reorganized.py

---
+++ caesar_cipher_reorganized.py
@@ -0,0 +1,18 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+ 
+def caesar(start_text, shift_amount, two_direction):
+  end_text = ""
+  if two_direction == "decode":
+      shift_amount *= -1
+  for letter in start_text: 
+    position = alphabet.index(letter)
+    new_position = position + shift_amount
+    new_letter = alphabet[new_position]
+    end_text += new_letter
+  print(f"The {direction} result is: {end_text}.")
+
+caesar(text, shift, direction)
